77e973fe9feda94bca4118f6d4ce2233215f3c11
souvik-ghosal-007 <souvik13.12.2000@gmail.com>
2023-08-25 17:24:35 +0530

    Added Domains for users and features

---
+++ src/main/java/com/souvik/freshvotes/domain/Feature.java
@@ -0,0 +1,45 @@
+package com.souvik.freshvotes.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Feature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
